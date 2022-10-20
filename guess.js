58669da4fc057db8fbffa093d96e5c85e7af03ff
Udonne Chinonso <nonso.udonne@gmail.com>
2022-10-20 20:09:28 +0000

    Added Readme file and comments

--- guess.js
+++ guess.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 
 
-// A Function 
+// A Function that contains the logic for the guess game and has the minimum and maximum values for the guess game as parameters.
 function guessGame(min, max){
     let safeword = 'end';
     let stage = 1;
@@ -11,29 +11,34 @@ function guessGame(min, max){
     let tries = 0;
 
 
-    let username = prompt('Please enter your username: ');
-    console.log(`Welcome to The Guessing Game ${username}! \nTo end the game, type "end" into the prompt. \nGoodluck!! \n\n`);
+    let username = prompt('Please enter your username to begin: ');
+  
+    console.log(`\nWelcome to The Guessing Game ${username}! \n\nTo end the game, type "end" into the prompt. \n\nGoodluck!! \n\n`);
 
 
     while(true){
+      // print out players current stage and points gained
         console.log(`Stage: ${stage} \nPoints: ${points} \n\n`);
 
+      // this ensures that the random number stays the same during a stage
         if(changeNum === 1){
             random_num = Math.floor(Math.random() * max) + 1;
             changeNum--;
         }
 
+      // prompt user for their guess
         let guess = prompt(`Predict a number between ${min} and ${max}: `);
 
-        
+        // check if player wants to end the game
         if (guess === safeword){
             console.log(`\nWell done ${username}! \nYou reached stage ${stage} and scored ${points} points with ${tries} tries! \nThanks for playing, see you next time ;)`);
             break;
         }
-        
-        guess = Number(guess);
-        console.log(guess, random_num, isNaN(guess));
 
+      // convert players guess to Number type 
+        guess = Number(guess);
+
+      // checks if correct or wrong 
         if(isNaN(guess)){
             console.log(`\nPlease enter a valid number within the range of ${min} and ${max} to play the game! \n`);
             continue;
@@ -57,5 +62,6 @@ function guessGame(min, max){
 }
 
 
-
+// function call to start game 
+// CHANGE RANGE VALUES HERE
 guessGame(1, 2)
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Guess Game
+
+##### The game prompts the user to guess a number within a certain range, if correct the user is awarded a point and promoted to the next stage but if wrong the user gets to try again. Happy Playing!
+
+
+### Some important notes on the project
+
+* The game has unlimited tries
+* The range values can be changed in the function call in guess.js
+* The random number to be guessed is generated once every stage by design
